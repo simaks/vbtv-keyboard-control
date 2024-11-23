cd0c317e2f3f2c436ccea75e5ec171d09348b1ee
Simas Skilinskas <simas.skilinskas@eneba.com>
2024-11-23 21:49:08 +0000

    add time information

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,96 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
+const formatTime = (seconds: number) => {
+  const h = Math.floor(seconds / 3600);
+  const m = Math.floor(seconds / 60) % 60;
+  const s = Math.floor(seconds % 60);
+  if (h) {
+    return [
+      h.toString(),
+      m.toString().padStart(2, "0"),
+      s.toString().padStart(2, "0"),
+    ].join(":");
+  } else {
+    return [m.toString().padStart(2, "0"), s.toString().padStart(2, "0")].join(
+      ":",
+    );
+  }
+};
+
 function App() {
-  const [seekTime, setSeekTime] = useState(10);
+  const [seekTime, setSeekTime] = useState<number>(10);
+  const [currentTime, setCurrentTime] = useState<number>();
+  const [duration, setDuration] = useState<number>();
+
+  const updateTime = () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const tab = tabs[0];
+      if (!tab.id) return;
+
+      chrome.scripting?.executeScript(
+        {
+          target: { tabId: tab.id },
+          func: () => {
+            const video = document.querySelector("video");
+            if (!video) return;
+            const { currentTime, duration } = video;
+            return { currentTime, duration };
+          },
+        },
+        (results) => {
+          const result = results[0]?.result;
+          if (!result) return;
+          if (result.currentTime != null) {
+            setCurrentTime(result.currentTime);
+          }
+          if (result.duration != null) {
+            setDuration(result.duration);
+          }
+        },
+      );
+    });
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(updateTime, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  });
 
   const seek = (seconds: number) => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      if (!tabs[0].id) return;
+      const tab = tabs[0];
+      if (!tab.id) return;
 
-      chrome.scripting.executeScript({
-        target: { tabId: tabs[0].id },
-        args: [seconds],
-        func: (funcSeconds) => {
-          const video = document.querySelector("video");
-          if (!video) return;
-          video.currentTime += funcSeconds;
-          if (video.paused) {
-            video.play();
+      chrome.scripting?.executeScript(
+        {
+          target: { tabId: tab.id },
+          args: [seconds],
+          func: (funcSeconds) => {
+            const video = document.querySelector("video");
+            if (!video) return;
+            const { currentTime, duration } = video;
+            video.currentTime += funcSeconds;
+            if (video.paused) {
+              void video.play();
+            }
+            return { currentTime, duration };
+          },
+        },
+        (results) => {
+          const result = results[0]?.result;
+          if (!result) return;
+          if (result.currentTime != null) {
+            setCurrentTime(result.currentTime);
+          }
+          if (result.duration != null) {
+            setDuration(result.duration);
           }
         },
-      });
+      );
     });
   };
 
@@ -48,6 +119,17 @@ function App() {
         <button onClick={backward}>{"<"}</button>
         <button onClick={forward}>{">"}</button>
       </div>
+
+      {currentTime && (
+        <div>
+          Current time: <div className="time">{formatTime(currentTime)}</div>
+        </div>
+      )}
+      {duration && (
+        <div>
+          Duration: <div className="time">{formatTime(duration)}</div>
+        </div>
+      )}
     </div>
   );
 }

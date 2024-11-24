fd204526c17eac5e10fb3e3974a5ac7518a621cc
Simas Skilinskas <simas.skilinskas@eneba.com>
2024-11-24 10:06:50 +0000

    icon and url checks

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,8 @@ function App() {
   const [seekTime, setSeekTime] = useState<number>(10);
   const [currentTime, setCurrentTime] = useState<number>();
   const [duration, setDuration] = useState<number>();
+  const [isInVbtv, setInVbtv] = useState<boolean>();
+  const [isInVbtvPlayer, setInVbtvPlayer] = useState<boolean>();
 
   const updateTime = () => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
@@ -32,21 +34,28 @@ function App() {
         {
           target: { tabId: tab.id },
           func: () => {
+            const vbtvPlayerUrl = "https://tv.volleyballworld.com/player";
+            const vbtvUrl = "https://tv.volleyballworld.com";
             const video = document.querySelector("video");
-            if (!video) return;
-            const { currentTime, duration } = video;
-            return { currentTime, duration };
+            const { currentTime, duration } = video ?? {};
+            const urlStartsWithVbtv = window.location.href.startsWith(vbtvUrl);
+            const urlStartsWithVbtvPlayer =
+              window.location.href.startsWith(vbtvPlayerUrl);
+            return {
+              currentTime: urlStartsWithVbtvPlayer ? currentTime : undefined,
+              duration: urlStartsWithVbtvPlayer ? duration : undefined,
+              isInVbtv: urlStartsWithVbtv,
+              isInVbtvPlayer: urlStartsWithVbtvPlayer,
+            };
           },
         },
         (results) => {
-          const result = results[0]?.result;
+          const result = results?.[0]?.result;
           if (!result) return;
-          if (result.currentTime != null) {
-            setCurrentTime(result.currentTime);
-          }
-          if (result.duration != null) {
-            setDuration(result.duration);
-          }
+          setCurrentTime(result.currentTime);
+          setDuration(result.duration);
+          setInVbtv(result.isInVbtv);
+          setInVbtvPlayer(result.isInVbtvPlayer);
         },
       );
     });
@@ -75,14 +84,14 @@ function App() {
             if (!video) return;
             const { currentTime, duration } = video;
             video.currentTime += funcSeconds;
-            if (video.paused) {
+            if (video.paused && funcSeconds > 0) {
               void video.play();
             }
             return { currentTime, duration };
           },
         },
         (results) => {
-          const result = results[0]?.result;
+          const result = results?.[0]?.result;
           if (!result) return;
           if (result.currentTime != null) {
             setCurrentTime(result.currentTime);
@@ -105,31 +114,45 @@ function App() {
 
   return (
     <div className="popup">
-      <label htmlFor="seekTime">Seek time</label>
-      <input
-        id="seekTime"
-        type="number"
-        min="1"
-        value={seekTime}
-        onChange={(event) => {
-          setSeekTime(event.target.valueAsNumber);
-        }}
-      />
-
-      <div className="seekButtons">
-        <button onClick={backward}>{"<"}</button>
-        <button onClick={forward}>{">"}</button>
-      </div>
-
-      {currentTime && (
-        <div>
-          Current time: <div className="time">{formatTime(currentTime)}</div>
-        </div>
+      {!isInVbtv && (
+        <p>
+          Go to{" "}
+          <a href="https://tv.volleyballworld.com" target="_blank">
+            VBTV
+          </a>
+        </p>
       )}
-      {duration && (
-        <div>
-          Duration: <div className="time">{formatTime(duration)}</div>
-        </div>
+      {isInVbtv && !isInVbtvPlayer && <p>Go to video page in VBTV</p>}
+      {isInVbtvPlayer && (
+        <>
+          <label htmlFor="seekTime">Seek time</label>
+          <input
+            id="seekTime"
+            type="number"
+            min="1"
+            value={seekTime}
+            onChange={(event) => {
+              setSeekTime(event.target.valueAsNumber);
+            }}
+          />
+
+          <div className="seekButtons">
+            <button onClick={backward}>{"<"}</button>
+            <button onClick={forward}>{">"}</button>
+          </div>
+
+          {currentTime != null && (
+            <div>
+              Current time:{" "}
+              <div className="time">{formatTime(currentTime)}</div>
+            </div>
+          )}
+          {duration != null && (
+            <div>
+              Duration: <div className="time">{formatTime(duration)}</div>
+            </div>
+          )}
+        </>
       )}
     </div>
   );
